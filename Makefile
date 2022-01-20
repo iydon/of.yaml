484641abee186c00a95299fe85acd93aa00f416b
Iydon Liang <liangiydon@gmail.com>
2022-01-21 02:56:32 +0800

    [update] conv sub-command set default version None

--- Makefile
+++ Makefile
@@ -2,6 +2,10 @@ POETRY = poetry
 PYTHON = $(POETRY) run python
 
 
+.PHONY: demo
+demo:
+	$(PYTHON) dictToFoam conv tutorials/incompressible/simpleFoam/airFoil2D.yaml
+
 .PHONY: test
 test:
 	for version in 7 ; do \
--- dictToFoam/__main__.py
+++ dictToFoam/__main__.py
@@ -21,16 +21,18 @@ def cli():
 @cli.command(help=Foam.__doc__)
 @click.argument('paths', nargs=-1)  # YAML format files or directories
 @click.option('-d', '--directory', default=DEFAULT.CONV_DIRECTORY, help='Destination directory')
-@click.option('-v', '--version', default=DEFAULT.OPENFOAM, help='OpenFOAM version')
+@click.option('-v', '--version', default=None, help='OpenFOAM version')
 @click.option('-o', '--exist-ok', is_flag=True, help='If `exist_ok` then do not overwrite')
 def conv(paths: t.Tuple[str, ...], directory: str, version: str, exist_ok: bool) -> None:
-    root = p.Path(directory, version)
+    root = p.Path(directory) if version is None else p.Path(directory, version)
     for path in map(p.Path, paths):
         if path.exists():
             if path.is_file():
                 dest = root / path.stem
                 if not (exist_ok and dest.exists()):
-                    Foam.from_file(path).save(dest)
+                    foam = Foam.from_file(path)
+                    if version is None or version in set(map(str, foam.meta.get('openfoam', []))):
+                        foam.save(dest)
             elif path.is_dir():
                 path = path.absolute()
                 for sth in path.glob('**/*'):
@@ -39,7 +41,7 @@ def conv(paths: t.Tuple[str, ...], directory: str, version: str, exist_ok: bool)
                             foam = Foam.from_file(sth)
                         except Exception:  # TODO: Foam.from_file exception
                             continue
-                        if version in set(map(str, foam.meta.get('openfoam', []))):
+                        if version is None or version in set(map(str, foam.meta.get('openfoam', []))):
                             name = '_'.join(sth.relative_to(path).parts)
                             dest = root / name
                             if not (exist_ok and dest.exists()):
