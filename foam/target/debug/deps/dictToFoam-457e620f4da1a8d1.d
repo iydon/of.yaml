5dbd53c6dee0dae9cfe2d2bce02e7bc06fec3d23
Iydon Liang <liangiydon@gmail.com>
2022-02-21 11:39:29 +0800

    :truck: Rename dictToFoam to foam

---
+++ foam/target/debug/deps/dictToFoam-457e620f4da1a8d1.d
@@ -0,0 +1,5 @@
+d:\Desktop\GitHub\of.yaml\dictToFoam\target\debug\deps\dictToFoam-457e620f4da1a8d1.rmeta: src\main.rs
+
+d:\Desktop\GitHub\of.yaml\dictToFoam\target\debug\deps\dictToFoam-457e620f4da1a8d1.d: src\main.rs
+
+src\main.rs:
---
+++ foam/target/debug/deps/dictToFoam-9765ed778991d3b3.d
@@ -0,0 +1,5 @@
+d:\Desktop\GitHub\of.yaml\dictToFoam\target\debug\deps\dictToFoam-9765ed778991d3b3.rmeta: src\main.rs
+
+d:\Desktop\GitHub\of.yaml\dictToFoam\target\debug\deps\dictToFoam-9765ed778991d3b3.d: src\main.rs
+
+src\main.rs:
