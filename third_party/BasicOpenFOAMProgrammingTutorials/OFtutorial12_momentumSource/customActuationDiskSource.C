be61e9daa728c9a19b736d86725245d50d3e84cf
Iydon Liang <liangiydon@gmail.com>
2022-05-05 00:03:44 +0800

    :white_check_mark: third_party: OFtutorial12_momentumSource

---
+++ third_party/BasicOpenFOAMProgrammingTutorials/OFtutorial12_momentumSource/customActuationDiskSource.C
@@ -0,0 +1,170 @@
+#include "customActuationDiskSource.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam {
+    namespace fv {
+            defineTypeNameAndDebug(customActuationDiskSource, 0);
+            addToRunTimeSelectionTable (
+                fvModel,
+                customActuationDiskSource,
+                dictionary
+            );
+        }
+}
+
+// * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+void Foam::fv::customActuationDiskSource::checkData() const {
+    if (magSqr(this->diskArea()) <= VSMALL) {
+        FatalErrorInFunction
+           << "diskArea is approximately zero"
+           << exit(FatalIOError);
+    }
+    if ((Cp_ <= VSMALL) || (Ct_ <= VSMALL)) {
+        FatalErrorInFunction
+           << "Cp and Ct must be greater than zero"
+           << exit(FatalIOError);
+    }
+    if (mag(diskDir_) < VSMALL) {
+        FatalErrorInFunction
+           << "disk direction vector is approximately zero"
+           << exit(FatalIOError);
+    }
+    if (returnReduce(upstreamCellId_, maxOp<label>()) == -1) {
+        FatalErrorInFunction
+           << "upstream location " << upstreamPoint_  << " not found in mesh"
+           << exit(FatalIOError);
+    }
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::fv::customActuationDiskSource::customActuationDiskSource (
+    const word& name, // name as appears in fvOptions dictionary
+    const word& modelType, // type of this instance
+    const dictionary& dict,
+    const fvMesh& mesh
+) :
+    // call the base class constructor
+    fvModel(name, modelType, dict, mesh),
+    // read control parameters from dictionary
+    Uname_(coeffs().lookup("Uname")),
+    diskDir_(coeffs().lookup("diskDir")),
+    diskCentre_(coeffs().lookup("diskCentre")),
+    D_(readScalar(coeffs().lookup("D"))),
+    t_(readScalar(coeffs().lookup("thickness"))),
+    Cp_(readScalar(coeffs().lookup("Cp"))),
+    Ct_(readScalar(coeffs().lookup("Ct"))),
+    upstreamPoint_(coeffs().lookup("upstreamPoint")),
+    // initialise other member fields
+    upstreamCellId_(-1),
+    V_(0.0)
+{
+    Info << tab << "- creating actuation disk zone: " << this->name() << endl;
+
+    // Locate the upstream cell used to apply velocity magnitude augment
+    upstreamCellId_ = mesh.findCell(upstreamPoint_);
+
+    // ===
+    // select cells forming part of the actuator disk
+
+    // make sure direction vector is unit length
+    diskDir_ /= mag(diskDir_);
+
+    // Create a dimensioned origin position to use with default OF mesh types
+    dimensionedVector x0 ("x0", dimLength, diskCentre_);
+
+    // compute distance and normal vectors from origin of the disk to use for selection
+    scalarField R (mag(mesh.C() - x0));
+    vectorField rHat ((mesh.C() - x0) / mag(mesh.C() - x0));
+
+    // go over each cell in the grid and comapre it against selection criteria
+    for (label cellI = 0; cellI < mesh.C().size(); cellI++) {
+        // determine distance from cell centre to disk axis and along the normal direction
+        scalar dNormal = R[cellI] * (rHat[cellI] & diskDir_);
+        scalar dRadial = sqrt(pow(R[cellI], 2.) - pow(dNormal, 2.));
+
+        // see if the cell is within tolerance from the centre of the disk
+        if ((mag(dNormal) < t_/2.) && (dRadial < D_/2.))
+            cells_.append(cellI);
+    }
+
+    // ===
+    // Set volume information
+    V_ = 0.0;
+    forAll(cells_, i)
+        V_ += mesh.V()[cells_[i]];
+    reduce(V_, sumOp<scalar>());
+
+    // ===
+    // validate inputs
+    checkData();
+
+    Info << tab
+         << "- selected " << returnReduce(cells_.size(), sumOp<label>())
+         << " cell(s) with volume " << V_ << endl;
+}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::wordList Foam::fv::customActuationDiskSource::addSupFields() const {
+    return wordList(1, Uname_);
+}
+
+void Foam::fv::customActuationDiskSource::addSup (
+    fvMatrix<vector>& eqn,
+    const word& fieldName
+) const {
+    const scalarField& cellsV = mesh().V();
+    vectorField& Usource = eqn.source();
+    const vectorField& U = eqn.psi();
+
+    if (V() > VSMALL) {
+        calculateMomentumSource (
+            Usource,
+            cells_,
+            cellsV,
+            geometricOneField(),
+            U
+        );
+    }
+}
+
+void Foam::fv::customActuationDiskSource::addSup (
+    const volScalarField& rho,
+    fvMatrix<vector>& eqn,
+    const word& fieldName
+) const {
+    const scalarField& cellsV = mesh().V();
+    vectorField& Usource = eqn.source();
+    const vectorField& U = eqn.psi();
+
+    if (V() > VSMALL) {
+        calculateMomentumSource (
+            Usource,
+            cells_,
+            cellsV,
+            rho,
+            U
+        );
+    }
+}
+
+bool Foam::fv::customActuationDiskSource::read(const dictionary& dict) {
+    if (fvModel::read(dict)) {
+        coeffs().readIfPresent("Uname", Uname_);
+        coeffs().readIfPresent("diskDir", diskDir_);
+        coeffs().readIfPresent("diskCentre", diskCentre_);
+        coeffs().readIfPresent("D", D_);
+        coeffs().readIfPresent("thickness", t_);
+        coeffs().readIfPresent("Cp", Cp_);
+        coeffs().readIfPresent("Ct", Ct_);
+
+        checkData();
+
+        return true;
+    } else {
+        return false;
+    }
+}
---
+++ third_party/BasicOpenFOAMProgrammingTutorials/OFtutorial12_momentumSource/customActuationDiskSourceTemplates.C
@@ -0,0 +1,60 @@
+#include "customActuationDiskSource.H"
+
+// * * * * * * * * * * * * * * *  Member Functions * * * * * * * * * * * * * //
+
+template<class RhoFieldType>
+void Foam::fv::customActuationDiskSource::calculateMomentumSource (
+    vectorField& Usource,
+    const labelList& cells,
+    const scalarField& Vcells,
+    const RhoFieldType& rho,
+    const vectorField& U
+) const {
+    // induced velocity coefficient
+    scalar a = 1.0 - Cp_/Ct_;
+
+    // find minimum velocity and density upstream of the actuator disk
+    vector upU = vector(VGREAT, VGREAT, VGREAT);
+    scalar upRho = VGREAT;
+    if (upstreamCellId_ != -1) {
+        upU =  U[upstreamCellId_];
+        upRho = rho[upstreamCellId_];
+    }
+    reduce(upU, minOp<vector>());
+    reduce(upRho, minOp<scalar>());
+
+    // resolve the upstream velocity to get component normal to the disk
+    upU *= (upU/mag(upU)) & (-1.*diskDir_);
+
+    // calculate thrust
+    // Units: [(kg m-3) * m2 * (m s-1) = (kg s-1)]
+    scalar T = 2.0*upRho*diskArea()*mag(upU)*a*(1 - a);
+
+    // distribute the total force around the disk proportionally to the volume of each cell
+    forAll(cells, i) {
+        // Units: [m3 * m-3 * (kg s-1) * (-) * (m s-1) = (kg m s-2) = (N)]
+        Usource[cells[i]] += ((Vcells[cells[i]]/V()) * T * diskDir_ * mag(upU));
+    }
+
+    // show a quick summary
+    Info << this->name() << ": added thrust = " << T << nl << endl;
+
+    // ===
+    // output a summary field highlighting active cells and showing thrust distribution
+    if (mesh().time().outputTime()) {
+        volVectorField momentumSourceField (
+            IOobject (
+                "momentumSourceField",
+                mesh().time().timeName(),
+                mesh(),
+                IOobject::NO_READ,
+                IOobject::AUTO_WRITE
+            ),
+            mesh(),
+            vector::zero
+        );
+        forAll(cells_, i)
+            momentumSourceField[cells[i]] = Usource[cells[i]];
+        momentumSourceField.write();
+    }
+}
