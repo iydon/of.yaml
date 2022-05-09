4d0677e4488c3f77a79ab2153612e5b62bf8aa2d
Iydon Liang <liangiydon@gmail.com>
2022-05-10 01:18:25 +0800

    :hammer: Software copyright application
    Generate documents required for software copyright application

--- Makefile
+++ Makefile
@@ -2,41 +2,46 @@ POETRY = poetry
 PYTHON = $(POETRY) run python
 
 
-.PHONY: help demo dependencies shell test standalone preview docs uncache publish
+.PHONY: help demo dependencies shell test standalone preview docs uncache publish copyright
 
-help:          ## Print the usage
+help:                  ## Print the usage
 	@fgrep -h "##" $(MAKEFILE_LIST) | fgrep -v fgrep | sed -e 's/\\$$//' | sed -e 's/##//'
 
-demo:          ## Run the demo code
+demo:                  ## Run the demo code
 	$(PYTHON) -m foam conv tutorials/incompressible/simpleFoam/airFoil2D.yaml
 
-dependencies:  ## List the dependencies as a tree
+dependencies:          ## List the dependencies as a tree
 	@$(POETRY) show --no-dev --tree
 
-shell:         ## Activate the virtual environment
+shell:                 ## Activate the virtual environment
 	@$(POETRY) shell
 
-test:          ## Run unit tests
+test:                  ## Run unit tests
 	for version in 7 ; do \
 		$(PYTHON) -m foam conv tutorials --directory test --version $$version --exist-ok ; \
 		$(PYTHON) -m foam test           --directory test --version $$version ; \
 	done
 
-standalone:    ## Convert Python package into a single file
-	@cp script/standalone.py .
-	@$(PYTHON) standalone.py
-	@rm standalone.py
+standalone:            ## Convert Python package into a single file
+	@cp script/$@.py .
+	@$(PYTHON) $@.py
+	@rm $@.py
 
-preview:       ## Run the builtin development server
+preview:               ## Run the builtin development server
 	$(PYTHON) -m mkdocs serve
 
-docs:          ## Build the MkDocs documentation
+docs:                  ## Build the MkDocs documentation
 	$(PYTHON) -m mkdocs Build
 
-uncache:       ## Remove __pycache__ directories
+uncache:               ## Remove __pycache__ directories
 	# https://stackoverflow.com/questions/28991015/python3-project-remove-pycache-folders-and-pyc-files
 	find . -type d -name  "__pycache__" -exec rm -r {} +
 
-publish:       ## Build and upload the package to a remote repository
+publish:               ## Build and upload the package to a remote repository
 	$(POETRY) build
 	$(POETRY) publish
+
+copyright: standalone  ## http://www.gov.cn/zhengce/2020-12/26/content_5574414.htm
+	@cp script/$@.py .
+	@$(PYTHON) $@.py
+	@rm $@.py
---
+++ script/copyright.py
@@ -0,0 +1,93 @@
+import pathlib as p
+import typing as t
+
+from docx import Document
+from docx.shared import Pt, RGBColor
+from pygments.lexers import guess_lexer_for_filename
+from pygments.styles import get_style_by_name
+
+
+Path = t.Union[str, p.Path]
+
+
+class Word:
+    '''
+    - Reference:
+        - http://www.gov.cn/zhengce/2020-12/26/content_5574414.htm
+    '''
+
+    Self = __qualname__
+
+    def __init__(
+        self,
+        root: Path = '.', style: str = 'default', page_break: bool = True,
+        font_name: str = 'Times New Roman', font_size: int = 9,
+    ) -> None:
+        self._root = p.Path(root).absolute()
+        self._default = {
+            'style': style,
+            'page_break': page_break,
+        }
+        self._doc = Document()
+        font = self._doc.styles['Normal'].font
+        font.name = font_name
+        font.size = Pt(font_size)
+
+    @classmethod
+    def new(cls, *args, **kwargs) -> Self:
+        return cls(*args, **kwargs)
+
+    @property
+    def root(self) -> p.Path:
+        return self._root
+
+    def add(
+        self,
+        path: Path, plain: bool = False,
+        style: t.Optional[str] = None, page_break: t.Optional[bool] = None,
+    ) -> Self:
+        if plain:
+            return self._add_plain(path, page_break)
+        path = p.Path(path).absolute()
+        code = path.read_text()
+        lexer = guess_lexer_for_filename(path.name, code)
+        styles = dict(get_style_by_name(style or self._default['style']))
+        # heading
+        self._doc.add_heading(path.relative_to(self._root).as_posix(), 0)
+        # paragraph
+        paragraph = self._doc.add_paragraph()
+        for type, value in lexer.get_tokens(code):
+            style = styles.get(type, {})
+            run = paragraph.add_run(value)
+            # bold, italic, underline
+            run.bold = style.get('bold', False)
+            run.italic = style.get('italic', False)
+            run.underline = style.get('underline', False)
+            # color
+            color = style.get('color', None)
+            if color is not None:
+                run.font.color.rgb = RGBColor.from_string(color)
+        # page break
+        if page_break or self._default['page_break']:
+            self._doc.add_page_break()
+        return self
+
+    def save(self, path: Path) -> Self:
+        self._doc.save(path)
+        return self
+
+    def _add_plain(self, path: Path, page_break: t.Optional[bool] = None) -> Self:
+        path = p.Path(path).absolute()
+        self._doc.add_heading(path.relative_to(self._root).as_posix(), 0)
+        self._doc.add_paragraph(path.read_text())
+        if page_break or self._default['page_break']:
+            self._doc.add_page_break()
+        return self
+
+
+if __name__ == '__main__':
+    word = Word.new(root='.', style='friendly', page_break=False, font_size=9)
+    for path in (word.root/'foam').rglob('*.py'):
+        word.add(path)
+    word.add(word.root/'foam.py')
+    word.save('copyright.docx')
