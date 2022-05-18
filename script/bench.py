b31648e2b23137daf4f5ddbf56fb7d5ab2b02bd1
Iydon Liang <liangiydon@gmail.com>
2022-05-18 19:32:34 +0800

    :hammer: Apply benchmark to parallel cases

--- script/bench.py
+++ script/bench.py
@@ -1,26 +1,38 @@
 import json
+import os
 import pathlib as p
 import timeit
+import typing as t
 
 from foam import Foam
 
 
-repeat = 99
-n_time = 999
+def is_valid(foam: Foam, threshold: t.Optional[int] = None) -> bool:
+    threshold = threshold or os.cpu_count()
+    if foam.number_of_processors == 1:
+        return True
+    elif foam.number_of_processors > threshold:
+        return False
+    else:
+        for static in foam['static']:
+            if static['name'] == 'Allrun':
+                break
+        else:
+            return False
+        return 'runParallel' in static['data']
+
+
+repeat = 7
+n_time = 19
 
 bench = {}
 for path in p.Path('tutorials').rglob('*.yaml'):
-    path = path.as_posix()
-    foam = Foam.from_file(path).save('case')
+    foam = Foam.from_file(path).save(f'case/{path.stem}')
     control = foam['foam']['system', 'controlDict']
-    if (
-        foam.number_of_processors == 1 and
-        '0' in foam['foam'] and
-        control['startFrom'] == 'startTime' and
-        control['stopAt'] == 'endTime'
-    ):
-        bench[path] = [[None, None] for _ in range(repeat)]
-        control['endTime'] = n_time * float(control['deltaT'])
+    if control['startFrom']=='startTime' and control['stopAt']=='endTime' and is_valid(foam, 4):
+        key = path.as_posix()
+        bench[key] = [[None, None] for _ in range(repeat)]
+        control['endTime'] = float(control['startTime']) + n_time*float(control['deltaT'])
         for ith in range(repeat):
             for jth, func in enumerate([
                 lambda: foam.cmd.raw('./Allrun'),
@@ -30,7 +42,7 @@ for path in p.Path('tutorials').rglob('*.yaml'):
                 tic = timeit.default_timer()
                 func()
                 toc = timeit.default_timer()
-                bench[path][ith][jth] = toc - tic
+                bench[key][ith][jth] = toc - tic
                 print(path, toc-tic)
 with open('bench.json', 'w') as f:
     json.dump(bench, f)
