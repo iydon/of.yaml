2577782bff0baa0a56fc873f2073b1df8e27ef70
Iydon Liang <liangiydon@gmail.com>
2022-10-27 16:08:34 +0800

    :alien: Update code due to external API changes

--- foam/__main__.py
+++ foam/__main__.py
@@ -10,8 +10,7 @@ from foam.base.type import TupleSequence
 class DEFAULT:
     '''Default arguments'''
 
-    CONV_DIRECTORY = '.'
-    TEST_DIRECTORY = 'test'
+    DIRECTORY = 'test'
     OPENFOAM = '7'
 
 
@@ -22,37 +21,48 @@ def cli() -> None:
 
 @cli.command(help=Foam.__doc__)
 @click.argument('paths', nargs=-1)  # YAML format files or directories
-@click.option('-d', '--directory', default=DEFAULT.CONV_DIRECTORY, help='Destination directory')
-@click.option('-v', '--version', default=None, help='OpenFOAM version')
+@click.option('-d', '--directory', default=DEFAULT.DIRECTORY, help='Destination directory')
+@click.option('-v', '--version', default=DEFAULT.OPENFOAM, help='OpenFOAM version')
 @click.option('-o', '--exist-ok', is_flag=True, help='If `exist_ok` then do not overwrite')
-def conv(paths: TupleSequence[str], directory: str, version: str, exist_ok: bool) -> None:
-    root = p.Path(directory) if version is None else p.Path(directory, version)
-    for path in map(p.Path, paths):
-        if path.exists():
-            if path.is_file():
-                dest = root / path.stem
+def conv(
+    paths: TupleSequence[str],
+    directory: str = DEFAULT.DIRECTORY,
+    version: str = DEFAULT.OPENFOAM,
+    exist_ok: bool = True,
+) -> None:
+    root = p.Path(directory, version)
+    for path in paths:
+        path = p.Path(path)
+        if path.is_dir():
+            return conv(
+                tuple(
+                    sth.as_posix()
+                    for sth in path.rglob('*')
+                    if sth.is_file() and sth.suffix in {'.yaml', '.yml'}
+                ), directory, version, exist_ok,
+            )
+        elif path.is_file():
+            try:
+                1 / 0
+                foam = Foam.from_path(path)
+            except Exception as e:
+                click.echo(f'{e!r}: {path}')
+                continue
+            if version in set(map(str, foam.meta.get('openfoam', []))):
+                dest = root / '-'.join(path.parts)
                 if not (exist_ok and dest.exists()):
-                    foam = Foam.from_file(path)
-                    if version is None or version in set(map(str, foam.meta.get('openfoam', []))):
-                        foam.save(dest)
-            elif path.is_dir():
-                path = path.absolute()
-                for sth in path.glob('**/*'):
-                    if sth.is_file():
-                        try:
-                            foam = Foam.from_file(sth)
-                        except Exception:  # TODO: Foam.from_file exception
-                            continue
-                        if version is None or version in set(map(str, foam.meta.get('openfoam', []))):
-                            name = '_'.join(sth.relative_to(path).parts)
-                            dest = root / name
-                            if not (exist_ok and dest.exists()):
-                                foam.save(dest)
+                    foam.save(dest)
+        else:
+            raise Exception(f'Path "{path.as_posix()}" does not exist, or is neither a file nor a directory')
+
 
 @cli.command()
-@click.option('-d', '--directory', default=DEFAULT.TEST_DIRECTORY, help='Directory containing YAML format files')
+@click.option('-d', '--directory', default=DEFAULT.DIRECTORY, help='Directory containing YAML format files')
 @click.option('-v', '--version', default=DEFAULT.OPENFOAM, help='OpenFOAM version')
-def test(directory: str, version: str) -> None:
+def test(
+    directory: str = DEFAULT.DIRECTORY,
+    version: str = DEFAULT.OPENFOAM,
+) -> None:
     '''For batch testing whether the converted files are operational'''
     errors = []
     root = p.Path(directory, version)
