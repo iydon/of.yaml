024340cfcc6166d5402f0a33e704cffd349601de
Iydon Liang <liangiydon@gmail.com>
2022-10-29 21:13:22 +0800

    :white_check_mark: Add unit-test for foam::parse::url::Url (TODO)

--- test/unit/parse/__init__.py
+++ test/unit/parse/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ['Test4Case', 'Test4Lark', 'Test4Static']
+__all__ = ['Test4Case', 'Test4Lark', 'Test4Static', 'Test4Url']
 
 
 from .case import Test as Test4Case
 from .lark import Test as Test4Lark
 from .static import Test as Test4Static
+from .url import Test as Test4Url
