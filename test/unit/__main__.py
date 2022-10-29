ffd45d999c5beb72fa769896d6032e8d34c9774e
Iydon Liang <liangiydon@gmail.com>
2022-10-29 13:55:32 +0800

    :building_construction: Make architectural changes for unit-test

--- test/unit/__main__.py
+++ test/unit/__main__.py
@@ -1,11 +1,6 @@
-__all__ = ['Test4Command', 'Test4Information', 'Test4PostProcess']
-
-
 import unittest
 
-from command import Test as Test4Command
-from information import Test as Test4Information
-from postprocess import Test as Test4PostProcess
+from app import *
 
 
 if __name__ == '__main__':
---
+++ test/unit/app/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ['Test4Command', 'Test4Information', 'Test4PostProcess']
+
+
+from .command import Test as Test4Command
+from .information import Test as Test4Information
+from .postprocess import Test as Test4PostProcess
